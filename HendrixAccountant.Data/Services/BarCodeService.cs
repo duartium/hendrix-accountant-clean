5edf282eee7e3e9ace513d956f5c5e04c40b9116
Byron Duarte <byronduarte95@gmail.com>
2021-03-31 23:40:51 -0500

    pruebas en creación de codigo de barras

--- HendrixAccountant.Data/Services/BarCodeService.cs
+++ HendrixAccountant.Data/Services/BarCodeService.cs
@@ -5,11 +5,13 @@ using BarcodeLib;
 using System.Drawing;
 using System.IO;
 using System.Drawing.Imaging;
+using System.Security.Permissions;
 
 namespace HendrixAccountant.Data.Services
 {
     public class BarCodeService : IBarCodeService
     {
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
         public void Generate(string code)
         {
 			try
@@ -18,11 +20,15 @@ namespace HendrixAccountant.Data.Services
                 barcode.IncludeLabel = true;
                 Image imgBarcode = barcode.Encode(TYPE.CODE128, code, Color.Black, Color.White, 300, 120);
 
-                string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + @"/NeutrinoTech/HendrixAccountant/Barcodes/";
-                
-                imgBarcode.Save(Path.Combine($"{folderPath}PROD_{code}.jpg"), ImageFormat.Jpeg);
+                string folderPath = @Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + "\\NeutrinoTech\\HendrixAccountant\\Barcodes\\";
+                string path = "C:\\HendrixBarcode\\";
+
+                string ruta = Path.Combine(folderPath, $"PROD_{ code}.jpg");
+                imgBarcode.Save(ruta, ImageFormat.Jpeg);
+
+
             }
-			catch (Exception ex)
+            catch (Exception ex)
 			{
                 Utils.GrabarLog("Generate", ex.ToString());
 			}
